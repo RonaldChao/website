6979ccf5f9cb1b57c03d724adb8eee84b818286a
waheng <peterchang@yahoo-inc.com>
2017-12-09 17:20:11 +0800

    Fix: css nude

--- src/serverRender.js
+++ src/serverRender.js
@@ -75,6 +75,7 @@ function renderFullPage(html, preloadedState) {
         <title>Cool Banana</title>
         <link rel="stylesheet" type="text/css" href=${bundleCSS}>
         <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+        <script src=${vendorJS}></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">        
       </head>
       <body>
@@ -84,7 +85,6 @@ function renderFullPage(html, preloadedState) {
           // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
-        <script src=${vendorJS}></script>
         <script src="/static/bundle.js"></script>
       </body>
     </html>
